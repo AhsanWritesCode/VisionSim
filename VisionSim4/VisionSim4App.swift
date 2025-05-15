7bf320126688c21138c34f81c22a30e77e346d98
Ahsan Tariq <ahsan.tariq@ucalgary.ca>
2025-05-15 14:56:33 -0600

    Initial Commit

---
+++ VisionSim4/VisionSim4App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSim4App.swift
+//  VisionSim4
+//
+//  Created by Ahsan Tariq on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct VisionSim4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionSim4Tests/VisionSim4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSim4Tests.swift
+//  VisionSim4Tests
+//
+//  Created by Ahsan Tariq on 2025-05-15.
+//
+
+import Testing
+@testable import VisionSim4
+
+struct VisionSim4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
