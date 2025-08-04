5760b1ad5a511184dcdcdba595e9cfcd1e357c59
“30119655” <ahsan.tariq@ucalgary.ca>
2025-08-04 10:02:44 -0600

    compiles now

--- VisionSim4/Model Views/EyeEntityView.swift
+++ VisionSim4/Model Views/EyeEntityView.swift
@@ -1,20 +1,15 @@
-//import SwiftUI
-//import RealityKit
-//import RealityKitContent
-//
-//
-//struct EyeEntityView: View {
-//    var configuration: EyeEntity.
-//
-//    var body: some View {
-//        RealityView { content in
-//            async let entityResult = Entity(named: "EyeModel", in: realityKitContentBundle)
-//
-//            if let entity = try? await entityResult {
-//                entity.setScale(SIMD3<Float>(0.01, 0.01, 0.01), relativeTo: nil)
-//                entity.position = SIMD3<Float>(0, -0.1, -0.2)
-//                content.add(entity)
-//            }
-//        }
-//    }
-//}
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct EyeEntityView: View {
+    var configuration: EyeEntity.Configuration
+    var animateUpdates: Bool = false // Optional, in case you add animations
+
+    var body: some View {
+        RealityView { content in
+            let eyeEntity = await EyeEntity(configuration: configuration)
+            content.add(eyeEntity)
+        }
+    }
+}
